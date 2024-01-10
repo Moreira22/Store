bec212de3d3b75aa4a5b5a21074fdff53678d819
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2024-01-10 15:19:01 -0300

    feat: add footer componemts

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useSession } from "next-auth/react";
-import Image from "next/image";
 import Categories from "./components/categories";
 import ProductListe from "./components/product-list";
 import { prismaClient } from "@/lib/prisma";
@@ -31,7 +30,7 @@ export default async function Home() {
   });
 
   return (
-    <div className=" flex flex-col gap-8">
+    <div className=" flex flex-col gap-8 py-8">
 
       <PromoBanner src="/banner-home-01.png" 
       alt="até 55% de Desconto só este mês"/>
