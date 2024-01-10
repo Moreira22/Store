7ff1db7130058aa202b5bc165c4a9412cd92a981
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2024-01-10 16:19:08 -0300

    feat: add category-item components to page category

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,6 +1,25 @@
-const CatalogPage = () => {
+import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
+import { ShapesIcon } from "lucide-react";
+import CategoryItem from "./components/category-item";
+
+const CatalogPage = async () => {
+    const category = await prismaClient.category.findMany({
+        where:{
+
+        }
+    })
     return ( 
-        <h1>catalogo</h1>
+        <div className="flex flex-col p-5 gap-8">
+            <Badge className="w-fit gap-1 text-base uppercase border-primary border-2 px-3 py-[0.375rem]"
+             variant="outline">
+                <ShapesIcon size={16}/>
+                Catálogo
+            </Badge>
+            <div className="grid grid-cols-2 gap-8">
+                {category.map(category => <CategoryItem key={category.id} category={category}/> )  }
+            </div>
+        </div>
      );
 }
  
