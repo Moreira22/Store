d8ef64107d8b893b28a1e55f56bc26c05f8926df
João Vitor Moreira <94926952+Moreira22@users.noreply.github.com>
2024-01-10 14:38:05 -0300

    feat: banner and pages

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -24,7 +24,7 @@ export default async function Home() {
         sizes="100vw"
         alt="até 55% de Desconto só este mês"
       />
-      <div className="mt-8 px-5">
+      <div className="mb-3 pl-5 font-bold uppercase mt-3">
         <Categories />
       </div>
       <div className="mt-6 px-5">
@@ -33,6 +33,14 @@ export default async function Home() {
       <div className="mt-8">
         <ProductListe  product={deals}/>
       </div>
+      <Image
+        src="/banner-home-02.png"
+        height={0}
+        width={0}
+        className="h-auto w-full px-5 mt-4"
+        sizes="100vw"
+        alt="até 55% de Desconto em mouses"
+      />
     </div>
   );
   // tempo : aula 2 => 01:19:41
